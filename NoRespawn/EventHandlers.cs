ce1be4f7ab6abf23682d4717bde4f708ff8b8652
Narin <narin4ikk@gmail.com>
2024-12-14 15:55:27 +0200

    Add project files.

---
+++ NoRespawn/EventHandlers.cs
@@ -0,0 +1,42 @@
+﻿using PlayerRoles;
+using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
+
+namespace NoRespawn
+{
+    public class EventHandlers
+    {
+        public void OnRespawningTeam(RespawningTeamEventArgs ev)
+        {
+            ev.IsAllowed = false;
+
+            var teamFaction = GetFaction(ev.NextKnownTeam);
+
+            if (teamFaction == Faction.FoundationStaff)
+            {
+                Log.Debug("Nine-Tailed Fox attempted to respawn but was prevented.");
+            }
+            else if (teamFaction == Faction.FoundationEnemy)
+            {
+                Log.Debug("Chaos Insurgency attempted to respawn but was prevented.");
+            }
+            else
+            {
+                Log.Debug($"An unknown team attempted to respawn: {ev.NextKnownTeam}.");
+            }
+
+            Cassie.Clear();
+        }
+
+        public void OnRespawnedTeam(RespawnedTeamEventArgs ev)
+        {
+            Cassie.Clear();
+        }
+
+        private Faction GetFaction(Faction nextKnownTeam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NoRespawn/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using Exiled.API.Features;
+using System;
+
+namespace NoRespawn
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "NoRespawn";
+        public override string Author => "Narin";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 0, 0);
+
+        private EventHandlers handlers;
+
+        public override void OnEnabled()
+        {
+            handlers = new EventHandlers();
+            Exiled.Events.Handlers.Server.RespawningTeam += handlers.OnRespawningTeam;
+            Exiled.Events.Handlers.Server.RespawnedTeam += handlers.OnRespawnedTeam;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RespawningTeam -= handlers.OnRespawningTeam;
+            Exiled.Events.Handlers.Server.RespawnedTeam -= handlers.OnRespawnedTeam;
+            handlers = null;
+            base.OnDisabled();
+        }
+    }
+}
